7ae96e01d3a59ef07e0f86e41c3379737259c2b4
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2021-10-30 18:40:34 -0300

    Create LoginIntefaz.py

---
+++ tp/loginIntento1/LoginIntefaz.py
@@ -0,0 +1,168 @@
+import csv
+
+
+#cambiar nombre
+class Error(Exception):
+    msg = "None pa ;)"
+
+
+def Choice():
+    while True:
+        try:
+                #yo pondria int(input()) y te ahorras todos los int(usuario)
+            usuario = int(input("Ingrese 1 para Ciudadano o  2 para Admin: "))
+            if usuario != 1 and usuario!=2:
+                raise Error()
+            break
+        except ValueError:
+                            # agregue 'alguno de los valores solicitados'
+            print("Ingrese alguno el valor solicitado")
+        except:
+            print("Ingrese uno de los valores especificados")
+    if usuario == 1:
+        CitizenLoop()
+    elif usuario == 2:
+        AdminLoop()
+    else:
+        print("Hay un error obteniendo usuario ;)")
+
+
+class ErrorCuil(Exception):
+    pass
+
+
+class ErrorTelefono(Exception):
+    pass
+
+class Checker:
+    @classmethod
+    def CheckCuil(self, cuil):
+        if len(cuil) == 11:
+            return True
+        else:
+            print("raro")
+            return False
+    @classmethod
+    def CheckDni(self, dni):
+        """Si es un dni retorna True"""
+        if len(dni) == 8:
+            return True
+        else:
+            return False
+    @classmethod
+    def CheckTelefono(self, telefono):
+        if len(telefono) == 10:
+            return True
+        else:
+            return False
+
+
+def CitizenLoop():
+    while True:
+        try:
+            # int(input())
+            accion = int(input("Si esta registrado presione 1, si quiere registrarse presione 2: "))
+            break
+        except ValueError:
+            print("Ingrese el numero solicitado")
+    if accion == 1:
+        CitizenLogIn()
+    elif accion == 2:
+        while True:
+            try:
+                nombre = input("Ingrese su nombre: ")
+                telefono = int(input("Ingrese su numero de telefono: +54 9 "))
+                                                              #no entiendo esto
+                if Checker.CheckTelefono(str(telefono)) == False:
+                    raise ErrorTelefono()
+                cuil = int(input("Ingrese su Cuil: "))
+                if Checker.CheckCuil(str(cuil)) == False:
+                    raise ErrorCuil()
+            except ValueError:
+                print("Coloque solo numeros enteros para el telefono y el cuil")
+            except Exception:
+                print("Un Cuil posee 11 digitos y un telefono 8 digitos ;)")
+            else:
+                registrarCiudadanoAnses(nombre.title(), int(telefono), int(cuil))
+                CitizenLoop()
+                break
+
+def verificar(nombre, telefono, cuil):
+    Anses = open("Anses.csv", "r")
+    reader = csv.reader(Anses, delimiter="|")
+    datos = [nombre, str(telefono), str(cuil)]
+    for fila in reader:
+        if fila[0] == datos[0] and fila[1] == datos[1] and fila[2] == datos[2]:
+            return True
+    Anses.close()
+    return False
+
+
+def registrarCiudadanoAnses(nombre, telefono, cuil):
+    verificacion = verificar(nombre, telefono, cuil)
+    if verificacion == True:
+        print("Ya existis ;)")
+    elif verificacion == False:
+        Anses = open("Anses.csv", "a", newline="")
+        writer = csv.writer(Anses, delimiter="|")
+        datos = [nombre, telefono, cuil]
+        writer.writerow(datos)
+        Anses.close()
+        print("Registrado")
+
+def CitizenLogIn():
+    while True:
+        try:
+            dato = input("Ingrese su Cuil: ")
+            if Checker.CheckCuil(str(dato)) == False:
+                raise ErrorCuil()
+            break
+        except ValueError:
+            print("Ingrese el valor solicitado")
+        except Exception:
+            print("Ingrese su Cuil de 11 digitos ;)")
+    print("Se ha iniciado sesion")
+    #Instanciar Ciudadano??!
+
+
+def CitizenChoices(ciudadano):
+    pass
+
+
+class NonAdminError(Exception):
+    msg = "None pa ;)"
+
+def AdminLoop():
+    counter = 0
+    while counter < 3:
+        try:
+            contrasena = input("Ingrese la contraseña de Administrador: ")
+            if contrasena == "Sumbudrule":
+                print("Creaste un Admin!!!!!!!!!")
+                return -1   #llamo a AdminChoices(admin)
+            elif contrasena != "Sumbudrule":
+                raise NonAdminError()
+            break
+        except:
+            print("Si sos admin, poné bien la contra ;)")
+            counter += 1
+    print("Vos no sos Admin, comunicate con un psicologo")
+    print("...Volviendo al inicio...")
+    Choice()
+
+
+def AdminChoices(admin):
+    pass
+
+Choice()
+
+
+#Como hacer para que no existan dos personas con el mismo cuil, mismo telefono, mismo nombre
+# Ciudadano choices y admin choices
+    # guarda con los loops y los breaks
+
+#PREGUNTA: Como hacer con los errorCuil y errorTelefono
+#ingles --> interfaz
+#español --> internas
+
+# valen --> hace un archivo con todas las funciones internas
