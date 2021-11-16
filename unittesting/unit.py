1652a1538c97e99ce60f7c0cd8dc23f7fbeb17dd
cataghiglione <92260181+cataghiglione@users.noreply.github.com>
2021-11-16 14:31:01 -0300

    Create unit.py

---
+++ unittesting/unit.py
@@ -0,0 +1,10 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        map = Function()
+        map.MapLoop()
+
+
+if __name__ == '__main__':
+    unittest.main()
