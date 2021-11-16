942bda47056c331e483180ab7a01955e0065c66a
cataghiglione <92260181+cataghiglione@users.noreply.github.com>
2021-11-16 14:34:53 -0300

    Create Reporte.py

---
+++ EventIt/Sensores/Reporte.py
@@ -0,0 +1,11 @@
+
+class Reporte:
+
+    def __init__(self, evento, fecha):
+        self.evento = evento
+        self.fecha = fecha
+
+    def getEvento(self):
+        return self.evento
+    def getFecha(self):
+        return self.fecha
