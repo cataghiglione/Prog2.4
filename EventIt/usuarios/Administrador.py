2eca96ba1b4374c441672368e36575da8fdafbe3
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2021-11-16 14:40:53 -0300

    Create Administrador.py

---
+++ EventIt/usuarios/Administrador.py
@@ -0,0 +1,18 @@
+class Administrador:
+    bloqueados = []
+    reportesAdministracionSensores = []
+
+    def __init__(self):
+        self.eventos = []
+
+    def bloquear(self, usuario):
+        usuario.bloqueado = True
+
+    def desbloquear(self, usuario):
+        usuario.bloqueado = False
+
+    def darAlta(self, evento):
+        pass
+
+    def auditar(self, usuarios):
+        pass
