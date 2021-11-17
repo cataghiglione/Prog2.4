c8335af1bdca219d80dd48d8860c15796eca29e6
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2021-11-16 21:03:33 -0300

    Create e.py

---
+++ EventIt/exceptions/e.py
@@ -0,0 +1,39 @@
+class Error(Exception):
+    @classmethod
+    def ErrorMsg(cls, msg):
+        return msg
+
+
+class ErrorCuil(Exception):
+    pass
+
+
+class ErrorTelefono(Exception):
+    pass
+
+
+class CiudadanoExisteMismoTelefonooNombre(Exception):
+    pass
+
+
+class CiudadanoExisteMismoCuil(Exception):
+    pass
+
+
+class FechaError(Exception):
+    pass
+
+
+class NonAdminError(Exception):
+    @classmethod
+    def NonAdminErrorMsg(cls, msg):
+        return msg
+
+
+class Bloqueado(Exception):
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def mensaje(self):
+        return self.msg
