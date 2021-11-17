e1f9592337d6d11d66b014c7c94db1b0cdc64a46
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2021-11-17 18:27:52 -0300

    Update Sensor.py

--- EventIt/Sensores/Sensor.py
+++ EventIt/Sensores/Sensor.py
@@ -1,10 +1,12 @@
 from EventIt.Sensores.Reporte import Reporte
 from EventIt.mapa1.Evento import Evento
 from EventIt.Sensores.AdministracionSensores import AdministracionSensores
+
+
 class Sensor:
 
     @classmethod
-    def ReprotarAdministracion(cls, x, y, nombre, tipo, cantidadPersonas, fecha):
+    def ReportarAdministracion(cls, x, y, nombre, tipo, cantidadPersonas, fecha):
         evento = Evento(x, y, nombre, tipo, cantidadPersonas)
         reporte = Reporte(evento, fecha)
         AdministracionSensores.eventosReportados.append(reporte)
