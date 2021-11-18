f4322eb9441dfd931b4ec7a4430e363ce9d407be
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2021-11-17 21:40:35 -0300

    Update solicitud.py

--- EventIt/solicitudes/solicitud.py
+++ EventIt/solicitudes/solicitud.py
@@ -1,4 +1,9 @@
-
 class Solicitud:
     def __init__(self, emisor):
         self.emisor = emisor
+
+    def __repr__(self):
+        return f"Solicitud de {self.emisor.nombre} con cuil {self.emisor.cuil}"
+
+    def getCuilEmisor(self):
+        return self.emisor.cuil
